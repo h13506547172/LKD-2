17291d2de45da80f9eedf4aa76ff53b22617b881
wudaidai <1665334372@qq.com>
2022-08-09 15:56:28 +0800

    完成

--- src/api/personnel.js
+++ src/api/personnel.js
@@ -33,17 +33,60 @@ export function delPersonnel(id){
 }
 
 // 工作量列表、
-export function getPersonnelWork(){
+export function getPersonnelWork(pageIndex,pageSize,userName,isRepair){
     return request({
-        url:'/user-service/user/searchUserWork'
+        url:'/user-service/user/searchUserWork',
+        params:{pageIndex,pageSize,userName,isRepair}
     })
 }
-
+export function getDataWork(start,end){
+    return request({
+        url:'/task-service/task/taskReportInfo/' + start + '/' + end,
+    })
+}
+ 
+// 获取工作量详情/user-service/user/    /task-service/task/taskReportInfo/:start/:end
+export function detailsApi(id){
+return request({
+    url:'/user-service/user/'+id
+})
+}
 
 // 商品搜索列表
 
-export function getVmservice(){
+export function getVmservice(pageIndex,pageSize,className){
     return request({
-        url:'/vm-service/skuClass/search'
+        url:'/vm-service/skuClass/search',
+        params:{pageIndex,pageSize,className}
     })
 }
+// 新增商品类型
+export function Addcommodity(data){
+    return request({
+        url:'/vm-service/skuClass',
+        method:'POST',
+        data
+    })
+}
+// 商品类型搜索
+export function getProductsearch(pageIndex,pageSize,className){
+    return request({
+        url:'/vm-service/skuClass/search',
+        params:{pageIndex,pageSize,className}
+    })
+}
+// 修改商品类型
+export function modifyApi(id,className){
+    return request({
+        url:'/vm-service/skuClass/'+ id,
+        method:'PUT',
+        data:{className}
+    })
+}
+// 删除商品类型
+export function delDeptsApi(id) {
+    return request({
+      url: "/vm-service/skuClass/" + id,
+      method: "DELETE",
+    });
+  }
